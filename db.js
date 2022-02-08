0c9e0bbf12a293bba8bc6dc7bfb39830a6dc08cb
RotemEzra1 <rotem19961@gmail.com>
2022-02-08 21:16:45 +0200

    try

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const uri = "mongodb+srv://204485320:204485320@cluster0.sqrqr.mongodb.net/ContactsDB?retryWrites=true&w=majority"
-// mongoose.connect(uri)
-// .then (() => console.log("con DB sucsses"))
-// .catch( err => console.log(err));
+const URI = "mongodb+srv://204485320:204485320@cluster0.sqrqr.mongodb.net/ContactsDB?retryWrites=true&w=majority"
+mongoose.connect(URI)
+.then (() => console.log("con DB sucsses"))
+.catch( err => console.log(err));
 
+module.exports = {
+    URI: process.env.URI
+}
